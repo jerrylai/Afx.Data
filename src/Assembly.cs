327ffcf43851c779495ba179c26d4da8ec088921
jerrylai <jerrylai@aliyun.com>
2021-02-18 11:52:02 +0800

    10.1.0

--- src/Assembly.cs
+++ src/Assembly.cs
@@ -6,5 +6,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © mycsv 2018")]
 [assembly: AssemblyTrademark("jerrylai@aliyun.com")]
 
-[assembly: AssemblyVersion("10.0.1")]
-[assembly: AssemblyFileVersion("10.0.1")]
+[assembly: AssemblyVersion("10.1.0")]
+[assembly: AssemblyFileVersion("10.1.0")]
--- version.cmd
+++ version.cmd
@@ -1,3 +1,3 @@
 @echo off
-set v=10.0.1
+set v=10.1.0
 tool\EditVersion dir="%cd%" v=%v% a="Assembly.cs"
