7d3d310a768d118870f4bbfba36eb2e2d827e01b
jerrylai <jerrylai@aliyun.com>
2022-07-29 10:32:35 +0800

    10.3.0

--- build.cmd
+++ build.cmd
@@ -1,14 +1,6 @@
 @echo off
-set Build="%SYSTEMDRIVE%\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\MsBuild.exe"
+set Build="%SYSTEMDRIVE%\Program Files\Microsoft Visual Studio\2022\Enterprise\MSBuild\Current\Bin\MsBuild.exe"
 if exist publish rd /s /q publish
-%Build% "NET20/Afx.Data/Afx.Data.csproj" /t:Rebuild /p:Configuration=Release
-%Build% "NET40/Afx.Data/Afx.Data.csproj" /t:Rebuild /p:Configuration=Release
-%Build% "NET45/Afx.Data/Afx.Data.csproj" /t:Rebuild /p:Configuration=Release
-%Build% "NET451/Afx.Data/Afx.Data.csproj" /t:Rebuild /p:Configuration=Release
-%Build% "NET452/Afx.Data/Afx.Data.csproj" /t:Rebuild /p:Configuration=Release
-%Build% "NET46/Afx.Data/Afx.Data.csproj" /t:Rebuild /p:Configuration=Release
-%Build% "NET461/Afx.Data/Afx.Data.csproj" /t:Rebuild /p:Configuration=Release
-%Build% "NET462/Afx.Data/Afx.Data.csproj" /t:Rebuild /p:Configuration=Release
 %Build% "NET47/Afx.Data/Afx.Data.csproj" /t:Rebuild /p:Configuration=Release
 %Build% "NET471/Afx.Data/Afx.Data.csproj" /t:Rebuild /p:Configuration=Release
 %Build% "NET472/Afx.Data/Afx.Data.csproj" /t:Rebuild /p:Configuration=Release
@@ -17,6 +9,4 @@ dotnet build "NETStandard2.0/Afx.Data/Afx.Data.csproj" -c Release
 dotnet build "NETStandard2.1/Afx.Data/Afx.Data.csproj" -c Release
 cd publish
 del /q/s *.pdb
-del /q/s net20\Afx.Base.dll
-del /q/s net20\Afx.Base.xml
 pause
